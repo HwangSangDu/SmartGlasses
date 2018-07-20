24c31da25c3c5aadcc13ef41d4f0eb40f1d357ea
Adam Hwang <ghkdtkden93@naver.com>
2018-07-20 21:49:45 +0900

    Update event, comment
    주석 처리
    이벤트 기반
    사진 필터링

--- Mask_RCNN/samples/demo.py
+++ Mask_RCNN/samples/demo.py
@@ -8,6 +8,38 @@
 # In[1]:
 
 
+C_END     = "\033[0m"
+C_BOLD    = "\033[1m"
+C_INVERSE = "\033[7m"
+ 
+C_BLACK  = "\033[30m"
+C_RED    = "\033[31m"
+C_GREEN  = "\033[32m"
+C_YELLOW = "\033[33m"
+C_BLUE   = "\033[34m"
+C_PURPLE = "\033[35m"
+C_CYAN   = "\033[36m"
+C_WHITE  = "\033[37m"
+ 
+C_BGBLACK  = "\033[40m"
+C_BGRED    = "\033[41m"
+C_BGGREEN  = "\033[42m"
+C_BGYELLOW = "\033[43m"
+C_BGBLUE   = "\033[44m"
+C_BGPURPLE = "\033[45m"
+C_BGCYAN   = "\033[46m"
+C_BGWHITE  = "\033[47m"
+
+
+
+
+def printComment(str):
+  print(C_BOLD +  C_GREEN + str + C_END)
+def printError(str):
+  print(C_BOLD +  C_RED + str + C_END)
+
+
+import re
 import time
 import os
 import sys
@@ -45,6 +77,10 @@ if not os.path.exists(COCO_MODEL_PATH):
 IMAGE_DIR = os.path.join(ROOT_DIR, "images")
 
 
+printComment("ROOT_DIR : " + ROOT_DIR)
+printComment("IMAGE_DIR : " + IMAGE_DIR)
+printComment("MODEL_DIR : " + MODEL_DIR)
+printComment("COCO_MODEL_PATH : " + COCO_MODEL_PATH)
 
 
 
@@ -56,21 +92,26 @@ def search(dir):
         #         print(file)
         return len(files)
 
-listLength = search(IMAGE_DIR)
-print(listLength)
 
 
 
 
-# 추가 시 길이에 변화가 생기면서 루프문을 벗어난다.
-# print("length checking...")
-# while (search(IMAGE_DIR)) == listLength:
-#  time.sleep(0.1)  # 0.1초
-#  continue
 
-# # 길이를 업데이트한다.
-# print("Add New Image File")
-# listLength = search(IMAGE_DIR)
+
+
+
+# while True :
+
+#   # 추가 시 길이에 변화가 생기면서 루프문을 벗어난다.
+#   printComment("Length Checking...")
+#   while (search(IMAGE_DIR)) == listLength:
+#    time.sleep(0.1)  # 0.1초
+#    printError("Time Sleep")
+#    continue
+#   # 길이를 업데이트한다.
+#   printComment("Add New Image File")
+#   listLength = search(IMAGE_DIR)
+#   print(listLength)
 
 
 
@@ -105,13 +146,8 @@ print(listLength)
 # search3(IMAGE_DIR)
 
 
-#command = "rm " + IMAGE_DIR + "/*.json"
-#print(command)
-#os.system(command)
-# sys.exit()
-command = "rm " + IMAGE_DIR + "/.DS_Store"
-print(command)
-os.system(command)
+
+
 
 
 
@@ -142,9 +178,11 @@ config.display()
 
 
 # Create model object in inference mode.
+printComment("Create Model Object")
 model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
 
 # Load weights trained on MS-COCO
+printComment("LOAD MS-COCO")
 model.load_weights(COCO_MODEL_PATH, by_name=True)
 
 
@@ -194,47 +232,94 @@ class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
 
 # In[5]:
 
+while True :
 
-# Load a random image from the images folder
-file_names = next(os.walk(IMAGE_DIR))[2]
+  listLength = search(IMAGE_DIR)
+  print(listLength)
+  # 추가 시 길이에 변화가 생기면서 루프문을 벗어난다.
+  printComment("Length Checking...")
+  while (search(IMAGE_DIR)) == listLength:
+   time.sleep(0.1)  # 0.1초
+   printError("Time Sleep")
+   continue
 
-print("선택 사진 : ", file_names[0])
-print()
-
-# image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
-
-## Read 1번째 사진
-print("read Image")
-image = skimage.io.imread(os.path.join(IMAGE_DIR, file_names[0]))
-
-## Run detection
-results = model.detect([image], verbose=1)
-
-## Visualize results (결과값을 저장하고 사진으로 띄워서 보여준다.)
-print(results)
-r = results[0]
-# visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
+  
+  # 길이를 업데이트한다.
+  printComment("Add New Image File")
+  listLength = search(IMAGE_DIR)
+  print(listLength)
 
 
-## Log print
-# print()
-# print("print R : ",r)
-# print()
-# print("print masks", r['masks'])
-# print()
-del r['masks']
-print(r)
-
-print()
-print("result : ", results)
+  ## Excute Command 
+  printComment("Remove /.DS_Store")
+  #command = "rm " + IMAGE_DIR + "/*.json"
+  #print(command)
+  #os.system(command)
+  command = "rm " + IMAGE_DIR + "/.DS_Store"
+  printComment(command)
+  os.system(command)
+  print()
 
 
+  ## Load a random image from the images folder
+  printComment("Load Image")
+  file_names = next(os.walk(IMAGE_DIR))[2]
+  printComment( "Selected Image : " + file_names[0])
+  print()
 
-## 사진을 지운다.
-# command = "rm " + IMAGE_DIR + "/*"
-# print(command)
-# os.system(command)
+  ## file_names Filtering
+  # file_names.extend(["1","2","3","4","5"])
+  print(file_names)
+  printComment("Filtering file_names")
+  # p = re.compile('\.(jpg|gif|png)')
+  p = re.compile('\w+\.(jpg|gif|png|jpeg)')
 
+
+  file_names = list(filter(p.match, file_names)) # Read Note
+  print(file_names)
+  printComment("Regex Completion")
+  # print(len(file_names))
+  # for i in range(0, len(file_names)): 
+  # for ele in file_names:
+  #   print(i)
+  #   res = p.match(file_names[i])
+  #   if(res == None):
+  #     file_names.pop(i)
+  # print(file_names)
+
+
+
+
+
+  ## Read 1번째 사진
+  printComment("read Image")
+  # image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+  image = skimage.io.imread(os.path.join(IMAGE_DIR, file_names[0]))
+
+
+  ## Run detection
+  results = model.detect([image], verbose=1)
+  printComment("Detect Completion")
+  r = results[0]
+
+
+  ## Visualize results (결과값을 저장하고 사진으로 띄워서 보여준다.)
+  # visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
+
+
+  ## Delete masks element
+  printComment("_DELETE mask")
+  del r['masks']
+  printComment("results : ")
+  print(r)
+
+
+  ## Erase Picture
+  command = "rm " + IMAGE_DIR + "/*"
+  print(command)
+  printComment("Erase Image DIR")
+  os.system(command)
+  time.sleep(0.1)  # 0.1초
 
 
 
